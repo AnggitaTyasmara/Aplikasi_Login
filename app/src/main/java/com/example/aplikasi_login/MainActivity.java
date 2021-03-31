d3e342cf9011a10ff59574ee3c39981566274d5d
AnggitaTyasmara <anggitotsmr@gmail.com>
2021-03-31 14:44:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/aplikasi_login/MainActivity.java
@@ -0,0 +1,108 @@
+package com.example.aplikasi_login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    //Deklarasi Variabel
+    Button btnLogin;
+
+    EditText edemail, edpassword;
+
+    String nama, password;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk menampilkan Menu.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        //Membuat kondisi jika yang dipilih adalah id mnDaftar
+        if (item.getItemId() == R.id.mnDaftar)
+        {
+            //Method untuk memanggil activity "DaftarActivity"
+            Intent i = new Intent(getApplicationContext(), DaftarActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Menghubungkan Variabel
+        btnLogin=findViewById(R.id.btSignin);
+
+        edemail=findViewById(R.id.edEmail);
+
+        edpassword=findViewById(R.id.edPassword);
+
+        //Membuat fungsi onclick
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //Menyimpan input user
+                nama = edemail.getText().toString();
+
+                password = edpassword.getText().toString();
+
+                String email="anggita@mail.com";
+                String pass = "1216";
+                //Mengecek apakah edittext email dan password terdapat isi atau tidak
+                if (nama.isEmpty() || password.isEmpty()){
+                    //membuat variabel toast dan menampilkan pesan "Edit text tidak boleh kosong"
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email dan Password wajib diisi!!!",
+                            Toast.LENGTH_LONG);
+                    //menampilkan Toast
+                    t.show();
+                } else {
+
+                    //Mengecek apakah isi dari email dan password sudah sama dengan email dan
+                    // password yang sudah diset
+                    if (nama.equals(email) && password.equals(pass))
+                    {
+                        //Membuat variabel toast dan menampilkan pesan "Login Sukses"
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Sukses", Toast.LENGTH_LONG);
+                        //menampilkan toast
+                        t.show();
+                        //Mmebuat object Bundle
+                        Bundle b = new Bundle();
+                        //memasukkan value dari variabel nama dengan kunci "a"
+                        // dan dimasukkan ke dalam bundle
+                        b.putString("a", nama.trim());
+                        //memasukkan value dari variabel nama dengan kunci "b"
+                        // dan dimasukkan ke dalam bundle
+                        b.putString("b", password.trim());
+                        //membuat objek intent berpindah activity dari mainactivity ke ActivityHasil
+                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
+                        //memasukkan bundle ke dalam intent untuk dikirimkan ke ActivityHasil
+                        i.putExtras(b);
+                        //berpindah ke ActivityHasil
+                        startActivity(i);
+
+                    } else{
+                        Toast t = Toast.makeText(getApplicationContext(),
+                                "Login Gagal", Toast.LENGTH_LONG);
+                        t.show();
+                    }
+                }
+            }
+        });
+    }
+}
